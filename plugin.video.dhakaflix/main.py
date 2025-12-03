ccdd48e9a875d29c7b127e0e38b1c12cb453f6dc
tazihad <tazihad@gmail.com>
2025-12-03 15:01:35 +0600

    added history

--- plugin.video.dhakaflix/main.py
+++ plugin.video.dhakaflix/main.py
@@ -4,17 +4,30 @@ import requests
 import xbmc
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
+import xbmcvfs 
 import os
 import json
+import time
 from urllib.parse import parse_qsl, quote, unquote, urlparse, urljoin
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # --- CONFIGURATION ---
-HANDLE = int(sys.argv[1])
+try:
+    HANDLE = int(sys.argv[1])
+except:
+    HANDLE = -1
+    
 BASE_URL = sys.argv[0]
 MAX_THREADS = 20
 
-# --- SEARCH CONFIGURATION (Updated to match Categories) ---
+# --- HISTORY CONFIGURATION ---
+ADDON = xbmcaddon.Addon()
+# We use xbmcvfs.translatePath for Kodi 19+ compatibility
+PROFILE_DIR = xbmcvfs.translatePath(ADDON.getAddonInfo('profile'))
+HISTORY_FILE = os.path.join(PROFILE_DIR, 'history.json')
+
+# --- SEARCH CONFIGURATION ---
 SEARCH_SERVERS = {
     'movies': [
         {'url': 'http://172.16.50.14', 'name': 'DHAKA-FLIX-14'},
@@ -53,10 +66,13 @@ SERIES_CATEGORIES = [
 # --- HELPER FUNCTIONS ---
 
 def clean_title(filename):
-    name = unquote(filename)
-    name = re.sub(r'\.(mkv|mp4|avi|flv|m4v)$', '', name, flags=re.IGNORECASE)
-    name = name.replace('.', ' ').replace('_', ' ').strip()
-    return name
+    try:
+        name = unquote(filename)
+        name = re.sub(r'\.(mkv|mp4|avi|flv|m4v)$', '', name, flags=re.IGNORECASE)
+        name = name.replace('.', ' ').replace('_', ' ').strip()
+        return name
+    except:
+        return filename
 
 def extract_meta(filename):
     name = clean_title(filename)
@@ -88,6 +104,76 @@ def extract_quality(filename):
     elif 'dvdrip' in fn: source = 'DVDRip'
     
     return f"{quality} {source}".strip()
+
+# --- HISTORY FUNCTIONS ---
+
+def load_history():
+    if not os.path.exists(HISTORY_FILE):
+        return []
+    try:
+        with open(HISTORY_FILE, 'r') as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_to_history(title, url, icon):
+    try:
+        if not os.path.exists(PROFILE_DIR):
+            os.makedirs(PROFILE_DIR)
+
+        history = load_history()
+        # Remove existing entry if it exists (to move it to top)
+        history = [h for h in history if h.get('url') != url]
+        
+        entry = {
+            'title': title, 
+            'url': url, 
+            'icon': icon, 
+            'time': int(time.time())
+        }
+        history.insert(0, entry)
+        history = history[:100] 
+        
+        with open(HISTORY_FILE, 'w') as f:
+            json.dump(history, f)
+    except:
+        pass
+
+def clear_history():
+    # Asks for confirmation then deletes the file
+    if xbmcgui.Dialog().yesno('DhakaFlix', 'Are you sure you want to clear Recently played media?'):
+        try:
+            if os.path.exists(HISTORY_FILE):
+                os.remove(HISTORY_FILE)
+            xbmcgui.Dialog().notification('DhakaFlix', 'History Cleared', xbmcgui.NOTIFICATION_INFO)
+            xbmc.executebuiltin('Container.Refresh')
+        except:
+            xbmcgui.Dialog().notification('DhakaFlix', 'Error Clearing History', xbmcgui.NOTIFICATION_ERROR)
+
+def history_menu():
+    history = load_history()
+    
+    if not history:
+        li = xbmcgui.ListItem("No Recently Played Media")
+        xbmcplugin.addDirectoryItem(HANDLE, "", li, isFolder=False)
+    else:
+        # --- NEW: CLEAR HISTORY BUTTON AT TOP ---
+        li = xbmcgui.ListItem("[COLOR red]Clear History[/COLOR]")
+        li.setArt({'icon': 'DefaultAddon.png', 'thumb': 'DefaultAddon.png'})
+        url = build_url("mode=clear_history")
+        xbmcplugin.addDirectoryItem(HANDLE, url, li, isFolder=False)
+        # ----------------------------------------
+
+        for item in history:
+            li = xbmcgui.ListItem(item['title'])
+            li.setInfo('video', {'title': item['title'], 'mediatype': 'video'})
+            li.setArt({'icon': item.get('icon', 'DefaultVideo.png'), 'thumb': item.get('icon', '')})
+            li.setProperty('IsPlayable', 'true')
+            
+            url = build_url(f"mode=play&url={quote(item['url'])}&title={quote(item['title'])}&icon={quote(item.get('icon', ''))}")
+            xbmcplugin.addDirectoryItem(HANDLE, url, li, isFolder=False)
+        
+    xbmcplugin.endOfDirectory(HANDLE)
 
 # --- NETWORK / SCRAPING (BROWSE MODE) ---
 
@@ -167,11 +253,8 @@ def get_smart_search_terms(query):
     words = [w for w in cleaned.split(' ') if len(w) > 2]
     
     terms = []
-    # 1. Full Title (Exact)
     terms.append(cleaned)
-    # 2. First 2 words
     if len(words) > 1: terms.append(" ".join(words[:2]))
-    # 3. First word (only if title was long)
     if len(words) > 2: terms.append(words[0])
         
     return list(dict.fromkeys(terms))
@@ -181,11 +264,8 @@ def search_runner(type_key, query):
     terms = get_smart_search_terms(query)
     all_results = []
     
-    # Try most specific term first
     for term in terms:
         term_results = []
-        
-        # Parallel search across all configured servers
         with ThreadPoolExecutor(max_workers=len(servers)) as executor:
             futures = [executor.submit(execute_single_search, term, srv) for srv in servers]
             for future in as_completed(futures):
@@ -194,7 +274,7 @@ def search_runner(type_key, query):
         
         if term_results:
             all_results = term_results
-            break # Stop if we found matches with specific term
+            break 
             
     return all_results
 
@@ -217,7 +297,6 @@ def display_search_results(type_key, query):
         xbmcplugin.endOfDirectory(HANDLE)
         return
 
-    # Deduplicate results based on URL
     seen = set()
     unique_results = []
     for r in results:
@@ -243,7 +322,7 @@ def display_search_results(type_key, query):
         li.setArt({'icon': 'DefaultVideo.png'})
         li.setProperty('IsPlayable', 'true')
         
-        url = build_url(f"mode=play&url={quote(item['fullUrl'])}")
+        url = build_url(f"mode=play&url={quote(item['fullUrl'])}&title={quote(title)}&icon=DefaultVideo.png")
         xbmcplugin.addDirectoryItem(HANDLE, url, li, isFolder=False)
         
     pDialog.close()
@@ -255,33 +334,26 @@ def build_url(query):
     return BASE_URL + '?' + query
 
 def main_menu():
-    import xbmcaddon
-    addon = xbmcaddon.Addon()
-    icon = addon.getAddonInfo('icon')
-    fanart = addon.getAddonInfo('fanart')
+    icon = ADDON.getAddonInfo('icon')
+    fanart = ADDON.getAddonInfo('fanart')
     
     items = [
         ("Movies", "movies_root"),
         ("TV Series", "series_root"),
         ("Search Movies", "search_input&type=movies"),
-        ("Search TV Series", "search_input&type=series")
+        ("Search TV Series", "search_input&type=series"),
+        ("Recently Played", "history")
     ]
 
     for name, mode in items:
         li = xbmcgui.ListItem(name)
         li.setArt({'icon': icon, 'thumb': icon, 'fanart': fanart})
         
-        # If mode contains parameters (like search_input&type=), handle correctly
         if '&' in mode:
-            # Split mode and params
-            # e.g. search_input&type=movies
             url = build_url(f"mode={mode}")
         else:
             url = build_url(f"mode={mode}")
             
-        # Search is a folder? No, Search opens input.
-        # However, to make it clickable in Kodi menu structure, we usually make it a directory item
-        # that calls the input function.
         xbmcplugin.addDirectoryItem(HANDLE, url, li, isFolder=True)
     
     xbmcplugin.endOfDirectory(HANDLE)
@@ -314,7 +386,6 @@ def browse(url):
     items.sort(key=lambda x: (not x['is_folder'], x['label'].lower()))
     folders = [i for i in items if i['is_folder']]
     
-    # Threaded Folder Image Scan
     folder_images = {}
     if folders:
         pDialog.update(10, 'Scanning folders...')
@@ -331,7 +402,6 @@ def browse(url):
                 except: pass
                 pDialog.update(int(10 + (i/len(folders)*80)))
 
-    # Local Files Scan
     local_poster = None
     local_subs = []
     for item in items:
@@ -364,10 +434,13 @@ def browse(url):
             if year: info['year'] = year
             li.setInfo('video', info)
             
-            if local_poster: li.setArt({'poster': local_poster, 'thumb': local_poster, 'fanart': local_poster})
-            else: li.setArt({'icon': 'DefaultVideo.png'})
+            icon_url = 'DefaultVideo.png'
+            if local_poster: 
+                icon_url = local_poster
+                li.setArt({'poster': local_poster, 'thumb': local_poster, 'fanart': local_poster})
+            else: 
+                li.setArt({'icon': 'DefaultVideo.png'})
             
-            # Subtitle matching
             base = os.path.splitext(item['label'])[0].lower()
             subs = []
             for s in local_subs:
@@ -377,13 +450,25 @@ def browse(url):
             if subs: li.setSubtitles(subs)
             
             li.setProperty('IsPlayable', 'true')
-            xbmcplugin.addDirectoryItem(HANDLE, build_url(f"mode=play&url={quote(item['url'])}"), li, isFolder=False)
+            
+            play_params = f"mode=play&url={quote(item['url'])}&title={quote(title)}&icon={quote(icon_url)}"
+            xbmcplugin.addDirectoryItem(HANDLE, build_url(play_params), li, isFolder=False)
             
     pDialog.close()
     xbmcplugin.setContent(HANDLE, 'movies')
     xbmcplugin.endOfDirectory(HANDLE)
 
-def play_video(url):
+def play_video(params):
+    url = params.get('url')
+    
+    if url:
+        try:
+            title = params.get('title', 'Unknown')
+            icon = params.get('icon', '')
+            save_to_history(title, url, icon)
+        except:
+            pass 
+
     li = xbmcgui.ListItem(path=url)
     xbmcplugin.setResolvedUrl(HANDLE, True, li)
 
@@ -400,11 +485,14 @@ def router(paramstring):
     elif mode == 'browse':
         browse(params.get('url'))
     elif 'search_input' in str(mode):
-        # Handle 'search_input' and 'search_input&type=x'
         type_key = params.get('type', 'movies')
         search_input(type_key)
     elif mode == 'play':
-        play_video(params.get('url'))
+        play_video(params)
+    elif mode == 'history':
+        history_menu()
+    elif mode == 'clear_history': 
+        clear_history()
 
 if __name__ == '__main__':
     router(sys.argv[2][1:])
